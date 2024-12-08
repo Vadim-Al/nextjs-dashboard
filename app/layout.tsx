6eae138045f26d69cf2336d1ef434fe08ecce9c8
Vadim A <177124158+Vadim-Al@users.noreply.github.com>
2024-12-08 10:35:02 +0200

    add clerk integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,24 @@
 import "@/app/ui/global.css"
-import { inter } from '@/app/ui/fonts';
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+import {inter} from "@/app/ui/fonts"
+import {
+	ClerkProvider,
+	SignInButton,
+	SignedIn,
+	SignedOut,
+	UserButton,
+} from "@clerk/nextjs"
+export default function RootLayout({children}: {children: React.ReactNode}) {
+	return (
+		<ClerkProvider>
+			<html lang="en">
+
+				<body className={`${inter.className} antialiased`}>
+        <header>
+				</header>
+
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
+	)
 }
